eaeb1c8d799e00715d6a955c85c3f2d9f8ba0f23
XunHao <hy_xunhao@163.com>
2018-03-29 15:01:12 +0800

    add.

--- 100 Cases/test9.py
+++ 100 Cases/test9.py
@@ -5,7 +5,7 @@
 
 import time
 
-myD = {1: 'a', 2: 'b'}
-for key, value in dict.items(myD):
+my = {1: 'a', 2: 'b'}
+for key, value in dict.items(my):
     print(key, value)
     time.sleep(1) # 暂停 1 秒
