3e063d4535786ebb86b97e7f6ea8a23596200ce0
XunHao <hy_xunhao@163.com>
2018-03-29 19:37:21 +0800

    add.

---
+++ 100 Cases/31-40/test31.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
+# Sunday Monday Tuseday Wednesday Thursday Firday Saturday
+
+buf = input('input:')
+
+if buf[0].isupper() == 'S':
+    if buf[1].lower() == 'u':
+        print("星期天")
+    elif buf[1].lower() == 'a':
+        print("星期六")
+    else:
+        print("Error.")
+elif buf[0].isupper() == 'M':
+    print("星期一")
+elif buf[0].isupper() == 'W':
+    print("星期三")
+elif buf[0].isupper() == 'T':
+    print("星期一")
