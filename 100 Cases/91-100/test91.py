f0711d3b793ad690bc45b4d77dfd622b04ead06a
XunHao <hy_xunhao@163.com>
2018-04-08 12:19:23 +0800

    add.

---
+++ 100 Cases/91-100/test91.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 时间函数举例1.
+
+import time
+
+print time.ctime(time.time())
+print time.asctime(time.localtime(time.time()))
+print time.asctime(time.gmtime(time.time()))
